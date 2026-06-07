c33fe9d6081515feeb1d7050bec81f78e18981aa
royalgames51 <royalgames2025@gmail.com>
2026-06-07 18:17:41 -0300

    awsminas

--- src/components/Juegos/Diamantes/diamantes.jsx
+++ src/components/Juegos/Diamantes/diamantes.jsx
@@ -8,7 +8,7 @@ const Diamantes = () => {
   const jugadorID = currentUser?.id || "default-id"; // Valor por defecto si no hay usuario
 
   // URL del juego con el jugadorID
-  const gameURL = `https://html-classic.itch.zone/html/17835802/minas261/index.html?jugadorID=${jugadorID}`;
+  const gameURL = `https://minas2royal.s3.us-east-2.amazonaws.com/minas261/index.html?jugadorID=${jugadorID}`;
 
   return (
     <Box
