86e98982a97d60c0b5edd5dae675d48c96d454ab
royalgames51 <royalgames2025@gmail.com>
2026-08-25 12:10:57 -0300

    pachinka

--- src/components/Juegos/royalpachinka/royalpachinka.jsx
+++ src/components/Juegos/royalpachinka/royalpachinka.jsx
@@ -7,7 +7,7 @@ const RoyalPachinka = () => {
   const jugadorID = currentUser?.id || "default-id";
   const gameURL = `https://royalpachinka.s3.us-east-2.amazonaws.com/pachinka/index.html?jugadorID=${jugadorID}`;
 
-  return <GameFrame src={jugadorID ? gameURL : null} title="Royal Pachinka" />;
+  return <GameFrame src={jugadorID ? gameURL : null} title="Royal Pachinka" nativeWidth={1920} nativeHeight={1080} />;
 };
 
 export default RoyalPachinka;
